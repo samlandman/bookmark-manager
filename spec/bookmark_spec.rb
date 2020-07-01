319864e257b2dd70d218fb3132fbc901b26637ae
sam landman <sam landman>
2020-07-01 14:34:13 +0100

    updated test

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,7 +1,4 @@
 require './lib/bookmark'
 
 describe Bookmark do
-  it 'should contain' do
-    expect(Bookmark.all.join).to eq("http://www.makersacademy.comhttp://www.google.comhttp://askjeeves.com")
-   end
 end
